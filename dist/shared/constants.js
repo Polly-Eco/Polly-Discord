de27b5295f5e951f6e9219ecab52f5a27a3f6fb3
Daniil.Vasylynenko <Daniil.Vasylynenko@nordstrom.com>
2022-09-03 10:56:39 +0300

    chore: remove dist

--- dist/shared/constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PATH_TO_COMMANDS = exports.CommandNames = void 0;
-var CommandNames;
-(function (CommandNames) {
-    CommandNames["COINFLIP"] = "coinflip";
-})(CommandNames = exports.CommandNames || (exports.CommandNames = {}));
-exports.PATH_TO_COMMANDS = 'commands';
-//# sourceMappingURL=constants.js.map
